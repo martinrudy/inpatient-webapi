c2b04a2802a1eb2d1f50eda3aa48525ebe7b5619
Martin  Rudolf <rudolfmato@gmail.com>
2024-05-22 16:14:07 +0200

    Added dockerization

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -36,6 +36,9 @@ switch ($command) {
     "test" {
         go test -v ./...
     }
+    "docker" {
+       docker build -t rudyni/inpatient-wl-webapi:local-build -f ${ProjectRoot}/build/docker/Dockerfile .
+    }
     default {
         throw "Unknown command: $command"
     }
